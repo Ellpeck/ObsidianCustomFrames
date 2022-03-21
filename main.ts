2ecd6662c9ddfc02dd0cf828e4f251d0bd0ebbda
Ellpeck <me@ellpeck.de>
2022-03-21 19:39:03 +0100

    actually allow setting settings to 0

--- main.ts
+++ main.ts
@@ -193,7 +193,7 @@ class KeepSettingTab extends PluginSettingTab {
 				t.inputEl.type = "number";
 				t.setValue(String(this.plugin.settings.minimumWidth));
 				t.onChange(async v => {
-					this.plugin.settings.minimumWidth = Number(v) || defaultSettings.minimumWidth;
+					this.plugin.settings.minimumWidth = v.length ? Number(v) : defaultSettings.minimumWidth;
 					await this.plugin.saveSettings();
 				});
 			});
@@ -204,7 +204,7 @@ class KeepSettingTab extends PluginSettingTab {
 				t.inputEl.type = "number";
 				t.setValue(String(this.plugin.settings.padding));
 				t.onChange(async v => {
-					this.plugin.settings.padding = Number(v) || defaultSettings.padding;
+					this.plugin.settings.padding = v.length ? Number(v) : defaultSettings.padding;
 					await this.plugin.saveSettings();
 				});
 			});
@@ -216,7 +216,7 @@ class KeepSettingTab extends PluginSettingTab {
 				t.inputEl.cols = 50;
 				t.setValue(this.plugin.settings.css);
 				t.onChange(async v => {
-					this.plugin.settings.css = v || defaultSettings.css;
+					this.plugin.settings.css = v.length ? v : defaultSettings.css;
 					await this.plugin.saveSettings();
 				});
 			});
